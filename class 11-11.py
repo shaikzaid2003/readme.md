c96bd30c05231cf3ec3a47dac9fa011f783e72fd
S ZAID <119155933+shaikzaid2003@users.noreply.github.com>
2022-12-04 15:08:06 +0530

    Add files via upload

---
+++ class 11-11.py
@@ -0,0 +1,20 @@
+'''a = input()
+b = ""
+c = ""
+for i in a:
+    if i.isupper():
+        b=b+i
+    else:
+        c=c+i
+print(b+c)'''
+
+
+
+
+'''a = input()
+b = a.swapcase()
+print(b)'''
+
+a = input()
+print(a[0]+a[len(a)//2]+a[-1])
+
